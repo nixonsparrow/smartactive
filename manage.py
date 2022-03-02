c17719ca0f2894dec92be007e30d7fbd0ca879b3
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-03-02 23:44:50 +0100

    Initial commit

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from dotenv import load_dotenv
+
+
+if __name__ == "__main__":
+    load_dotenv('.env')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartmove.settings.base")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
