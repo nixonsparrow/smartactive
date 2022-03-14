12f4d8ac6a4838555c85a04f8f2395b13572c65e
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-03-14 22:42:48 +0100

    Payments

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'amount', 'user', 'updated_at', 'created_at']
+
+
+admin.site.register(Payment, PaymentAdmin)
--- payments/models.py
+++ payments/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from manager.models import TimestampModel
+from users.models import User
 
 
 class Payment(TimestampModel):
     amount = models.DecimalField(_('Amount'), max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, verbose_name=_('User'), on_delete=models.SET_NULL,
+                             related_name='payments', null=True, blank=True)
