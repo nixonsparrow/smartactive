726fe630861d0dd6002b17b0529841573b4b0029
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-03-14 20:53:56 +0100

    Create manager app

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from manager.models import TimestampModel
+
+
+class Payment(models.Model, TimestampModel):
+    amount = models.DecimalField(_('Amount'), max_digits=10, decimal_places=2)
