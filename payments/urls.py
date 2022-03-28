b0473b7c0210d7869117ac8f3a179e691c0dfd24
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-03-29 00:14:55 +0200

    Tests Fix

--- payments/urls.py
+++ payments/urls.py
@@ -5,7 +5,7 @@ from payments import views
 app_name = 'payments'
 
 urlpatterns = [
-    path('', views.Overview.as_view(), name='overview'),
+    path('', views.Overview.as_view(), name='all'),
     path('new/', views.PaymentCreateView.as_view(), name='new'),
     path('update/<int:pk>/', views.PaymentUpdateView.as_view(), name='update'),
 ]
